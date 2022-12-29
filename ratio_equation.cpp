620962763e7670eacb01c547dc6cca623c2bff37
welldone <amirvaladan1993@gmail.com>
2022-12-29 18:49:28 +0330

    this is solution of ratio equation

---
+++ ratio_equation.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+main( ){
+	float num1,num2,num3,x;
+	
+		cout<<"if (a/c)=(b/x)\n";
+		
+		cout<<"enter a:\n";
+		cin>>num1;
+		
+		cout<<"enter b:\n";
+		cin>>num2;
+		
+		cout<<"enter c:\n";
+		cin>>num3;
+	
+		x=num2*num3/num1;
+		cout<<"x =  "<<x;
+		
+	return 0;
+}
