977322e466b2d734872f169907e8820794b42147
welldone <amirvaladan1993@gmail.com>
2022-12-29 19:23:16 +0330

    showing ASCII of charachter

---
+++ show_ascii_of_charachter.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+main( ){
+ 
+	char charachter; 
+	float ascii;
+
+	cout<<"Enter one character :   ";
+	cin>>charachter;
+	
+	ascii=(int) charachter; 
+	
+	cout<<"ASCII code is :   "<<ascii;
+
+    return 0;
+}
