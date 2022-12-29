eb5ea15c61e250afe0172bda04bf9bd1de7174f0
welldone <amirvaladan1993@gmail.com>
2022-12-29 19:16:50 +0330

    power of 2 of entry number

---
+++ power_of_2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+main( ){
+	int num;
+		cout<<"Enter the number:";
+		cin>>num;
+	
+		num=num*num;
+		
+		cout<<"The power of 2 of your number is   :   "<<num;
+	
+	return 0;
+}
