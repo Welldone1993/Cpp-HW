e957efb6d3e9a00a877e95f35438f4cd62f7a69f
welldone <amirvaladan1993@gmail.com>
2023-01-01 15:18:52 +0330

    insertion algorithm

---
+++ Class_works/insertionSort.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int isArray[5] = {4,6,2,5,1};
+	int isSort[5];
+	int temp;
+	
+	for(int i=1;i<5;i++){
+		
+		for(int j=i;j>0;j--){
+			
+			if(isArray[j]<isArray[j-1]){
+				
+				temp=isArray[j];
+				isArray[j]=isArray[j-1];
+				isArray[j-1]=temp;
+			}
+		}
+	}
+	for(int x=0;x<5;x++){
+				cout<<isArray[x]<<"\n";
+			}
+	return 0;
+}
